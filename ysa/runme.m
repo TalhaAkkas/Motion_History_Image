66f44c1c09833894ae37bb97f5d875e5658093e8
Talhaakkas <Talhaakkas@gmail.com>
2013-06-06 00:21:03 +0300

    last touc

--- ysa/runme.m
+++ ysa/runme.m
@@ -2,44 +2,50 @@ close all; clear all; clc;
 dbg = ~true;
 db = '../_db/';
 db_64x64_fg = strcat(db, '64x64/Our_Foreground/');
+db_mhi = strcat(db, 'MHI/');
+db_mei = strcat(db, 'MEI/');
+
 
 dip_initialise('silent');
 
 %% Datastore
-load(strcat(db, 'features.mat'));
-
-%% Seperation Points
-% Ground Truth'lari yukle ve uzerine hesaplananlari ekle
-%load(strcat(db, 'seperation_points_ground_truth.mat'));
-pro = []
-tr = []
-psz = size(features, 3);
+load(strcat(db, 'datastore.mat'));
+objectsMHI = []
+objectsMEI = []
+targets = []
+psz = size(vf_fnms, 3);
 for pi=1:psz
     if true, fprintf('%03d/%03d. kisi isleniyor...\n', pi, psz); end
 
-    ssz = cellsize(features, pi);
+    ssz = cellsize(vf_fnms, pi);
     for si=1:ssz
         if true, fprintf('\t%1d/%1d. seq isleniyor...\n', si, ssz);  end
 
         clear seq_frms;
-        fsz = cellsize(features, pi, si);
-        for fi=1:fsz
-            if true, fprintf('\t\t%03d/%03d. frame isleniyor...\n', fi, fsz);    end
+        fsz = cellsize(vf_fnms, pi, si);
             
-            bw = features{fi, si, pi}.project;
-            if(si == 2)
-            pro = [pro; reshape(bw,1,[])];
-            tr = [tr; 1,1];
-            elseif(si == 6)
-            pro = [pro; reshape(bw,1,[])];
-            tr = [tr; 1,0];
+            [t, nm] = fileparts(vf_fnms{fsz, si, pi});
+            nm(1) ='f'
+            nm(2) ='g'
+            mhi = imread(strcat(db_mhi, strcat(nm, '.png')));
+            mei = imread(strcat(db_mei, strcat(nm, '.png')));
+            
+            if(si == 1)
+                objectsMHI = [objectsMHI; reshape(mhi,1,[])];
+                objectsMEI = [objectsMEI; reshape(mei,1,[])];
+                targets = [targets; 0,1];
+            elseif(si == 4)
+                objectsMHI = [objectsMHI; reshape(mhi,1,[])];
+                objectsMEI = [objectsMEI; reshape(mei,1,[])];
+                targets = [targets; 0,1];
                 
-            elseif(si == 8)
-            pro = [pro; reshape(bw,1,[])];
-            tr = [tr; 0,1];
+            elseif(si == 9)
+                objectsMHI = [objectsMHI; reshape(mhi,1,[])];
+                objectsMEI = [objectsMEI; reshape(mei,1,[])];
+                targets = [targets; 0,1];
             end
-        end
     end
 end
-save('tr.mat','tr')
-save('pro.mat','pro')
+save('targets.mat','targets')
+save('objectsMHI.mat','objectsMHI')
+save('objectsMEI.mat','objectsMEI')
